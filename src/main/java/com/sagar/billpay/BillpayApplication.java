67b5cf35cdbe95edc01b99149c3261cc9540d5a1
sagar sonawane <sagar10483@gmail.com>
2022-12-16 00:45:32 +0530

    Test

---
+++ src/main/java/com/sagar/billpay/BillpayApplication.java
@@ -0,0 +1,14 @@
+package com.sagar.billpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillpayApplication.class, args);
+	}
+
+	
+}
